ed3f8f651142b30e1cf725f7100cb324b5d90224
joe-meyer <joemeyer@exchangecore.com>
2015-11-20 09:56:43 -0500

    got concrete5magic styles working, need to get the snippets still

--- assets/concrete5styles/plugin.js
+++ assets/concrete5styles/plugin.js
@@ -13,14 +13,30 @@
 
                 success: function(response) {
                     var additionalStyles = [];
-                    jQuery.each(response.classes, function()
+
+                    $.each(response.snippets, function(i, snippet) {
+                        editor.widgets.add( snippet.scsHandle, {
+                            template: snippet.scsName
+                        });
+                        var style = {};
+                        style.name = snippet.scsName;
+                        style.element = 'span';
+                        style.attributes = {
+                            'class': 'ccm-content-editor-snippet',
+                            'contenteditable': 'false',
+                            'data-scsHandle': snippet.scsHandle,
+                        };
+                        additionalStyles.push(style);
+                    });
+
+                    $.each(response.classes, function()
                     {
                         var style = {};
                         style.name = this.title;
                         if (typeof this.forceBlock !== 'undefined' && this.forceBlock == 1) {
-                            this.element = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p'];
+                            style.element = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p'];
                         } else {
-                            this.element = 'span';
+                            style.element = 'span';
                         }
                         if (typeof this.spanClass !== 'undefined') {
                             style.attributes = {'class': this.spanClass};
